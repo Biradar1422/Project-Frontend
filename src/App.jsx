c34e86ddc8bae330b480b105867a6feb35c356ba
priya <priya.biradar14@gmail.com>
2024-07-19 13:34:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState([])
+
+  const getData = async ()=>{
+    const mainData = await axios.get("http://localhost:4000/courses")
+    console.log(mainData.data)
+    setData(mainData.data)
+  }
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
+  return (
+    <>
+    <table border={1}>
+      <tr>
+        <th>Id</th>
+        <th>courseName</th>
+      </tr>
+      {
+        data.map((val, index)=>{
+          return(
+            <tr key={val.id}>
+              <td>{index+1}</td>
+              <td>{val.courseName}</td>
+            </tr>
+          )
+        })
+      }
+    </table>
+      
+    </>
+  )
+}
+
+export default App
